0bbf233fce9a2dd78098bfd6233e6f78f18cdddc
DhruvRaiDh <DhruvRaiDh@users.noreply.github.com>
2026-03-25 10:05:11 +0530

    Align tkinter hook with PyInstaller runtime expectations

--- hooks/hook-tkinter.py
+++ hooks/hook-tkinter.py
@@ -3,13 +3,11 @@ import sys
 
 hiddenimports = ["tkinter"]
 
-_datas = []
+datas = []
 tcl_dir = os.path.join(sys.base_prefix, "tcl", "tcl8.6")
 tk_dir = os.path.join(sys.base_prefix, "tcl", "tk8.6")
 
 if os.path.isdir(tcl_dir):
-    _datas.append((tcl_dir, "tcl/tcl8.6"))
+    datas.append((tcl_dir, "_tcl_data"))
 if os.path.isdir(tk_dir):
-    _datas.append((tk_dir, "tcl/tk8.6"))
-
-datas = _datas
+    datas.append((tk_dir, "_tk_data"))
